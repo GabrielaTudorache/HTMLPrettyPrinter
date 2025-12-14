5bc9bd62c5229a3e13b4d4fa34dd7cd9ca34aa98
Gabriela Tudorache <gabrielatudorache673@gmail.com>
2025-12-14 09:18:00 +0200

    Implement correct indentation for self-closing and void HTML elements

--- html_formatter.sh
+++ html_formatter.sh
@@ -1,6 +1,51 @@
 #!/bin/bash
 
 INDENT_STRING="  "
+VOID_ELEMENTS=(
+    "area"
+    "base"
+    "br"
+    "col"
+    "embed"
+    "hr"
+    "img"
+    "input"
+    "link"
+    "meta"
+    "param"
+    "source"
+    "track"
+    "wbr"
+)
+
+# extract tag name from a tag string
+# Ex: <div class="test"> -> div
+#     </div> -> div
+#     <br/> -> br
+get_tag_name() {
+    local tag="$1"
+    # remove < and </ from the start
+    local name="${tag#</}"
+    name="${name#<}"
+    # remove everything after the first space, > or /
+    name="${name%% *}"
+    name="${name%%>*}"
+    name="${name%%/*}"
+    # convert to lowercase for comparison
+    echo "${name,,}"
+}
+
+# check if a tag is a void element
+is_void_element() {
+    local tag_name="$1"
+    local elem
+    for elem in "${VOID_ELEMENTS[@]}"; do
+        if [[ "${tag_name,,}" == "$elem" ]]; then
+            return 0  # true
+        fi
+    done
+    return 1  # false
+}
 
 # function to print indentation
 print_indent() {
@@ -52,7 +97,20 @@ main() {
                 # opening tag - print then increase indent
                 print_indent $indent_level
                 echo "$line"
-                ((indent_level++))
+                
+                # extract tag name and check if it's a void element
+                local tag_name=$(get_tag_name "$line")
+                
+                if [[ "$line" =~ /\>$ ]]; then
+                    # self-closing tag, don't increase indent
+                    :
+                elif is_void_element "$tag_name"; then
+                    # void element, don't increase indent
+                    :
+                else
+                    # regular opening tag, increase indent
+                    ((indent_level++))
+                fi
             else
                 # text content
                 print_indent $indent_level
